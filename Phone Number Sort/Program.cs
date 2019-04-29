d72548e4f074733b8e96ce490dc0077527ef3e2a
Erik Madsen <ekmadsen@comcast.net>
2019-04-29 09:16:30 -0500

    Added Phone Number Sort project.

---
+++ Phone Number Sort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Phone_Number_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
